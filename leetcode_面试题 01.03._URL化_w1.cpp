f0f7bccc3b70290e751ed632fd2ec2c4f94ac434
BestBlade <68151785+BestBlade@users.noreply.github.com>
2021-04-29 15:28:00 +0800

    Add files via upload

---
+++ leetcode_面试题 01.03._URL化_w1.cpp
@@ -0,0 +1,85 @@
+﻿/* ------------------------------------------------------------------|
+URL化。编写一种方法，将字符串中的空格全部替换为%20。假定该字符串尾部有足够的空间存放新增字符，并且知道字符串的“真实”长度。（注：用Java实现的话，请使用字符数组实现，以便直接在数组上操作。）
+
+ 
+
+示例 1：
+
+输入："Mr John Smith    ", 13
+输出："Mr%20John%20Smith"
+示例 2：
+
+输入："               ", 5
+输出："%20%20%20%20%20"
+ 
+
+提示：
+
+字符串长度在 [0, 500000] 范围内。
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/string-to-url-lcci
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。|
+-------------------------------------------------------------------*/
+
+/*	双指针法
+*
+*	执行用时：28 ms, 在所有 C++ 提交中击败了99.57%的用户
+*	内存消耗：21.6 MBB, 在所有 C++ 提交中击败了74.65%的用户
+*/
+
+#include <iostream>
+#include <string> 
+#include <vector>
+#include <algorithm>
+#include <unordered_map>
+#include <map>
+#include <stack>
+#include <queue>
+
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode* next;
+    ListNode(int x) : val(x), next(NULL) {}
+
+};
+
+struct TreeNode {
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+class Node {
+public:
+    int val;
+    Node* left;
+    Node* right;
+    Node* next;
+
+    Node() : val(0), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val) : val(_val), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val, Node* _left, Node* _right, Node* _next)
+        : val(_val), left(_left), right(_right), next(_next) {}
+};
+
+string replaceSpaces(string S, int length) {
+    int n = S.size() - 1;
+    for (int i = length - 1; i >= 0; --i) {
+        if (S[i] == ' ') {
+            S[n--] = '0';
+            S[n--] = '2';
+            S[n--] = '%';
+        }
+        else {
+            S[n--] = S[i];
+        }
+    }
+    //避免后续有多余空格
+    return S.substr(n + 1);
+}
